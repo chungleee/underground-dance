cc2a000339520d4458bdc0a1bec8fe4ae8dc7d0c
Leon <leon.karho.chung@gmail.com>
2020-07-02 14:17:59 -0400

    customizing Drawer width & welcome logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { Drawer, Icon, List, ListItem, Typography } from "@material-ui/core";
+import {
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItem,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  paper: {
+    minWidth: "300px",
+  },
+  welcomeLogo: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: "2em",
+    paddingTop: "2em",
+  },
+});
 
 const Sidebar = () => {
+  const classes = useStyles();
   const listItems = [
     {
       icon: "fas fa-home",
@@ -17,11 +40,16 @@ const Sidebar = () => {
     },
   ];
   return (
-    <Drawer variant="permanent" anchor="left">
-      <Typography>
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      classes={{ paper: classes.paper }}
+    >
+      <div className={classes.welcomeLogo}>
         <Icon className="fas fa-rocket" />
-        Welcome, Ryan
-      </Typography>
+        <Typography>Welcome, Ryan</Typography>
+      </div>
+      <Divider />
       <List>
         {listItems.map((item) => {
           return (
