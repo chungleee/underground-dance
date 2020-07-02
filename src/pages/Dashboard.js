60028131aeca53d58af566cf0bd844511fb6946a
Leon <leon.karho.chung@gmail.com>
2020-07-02 13:31:11 -0400

    import Sidebar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,20 +1,10 @@
 import React from "react";
-import { Drawer, Icon, List, ListItem, Typography } from "@material-ui/core";
+import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
   return (
     <>
-      <Drawer variant="permanent" anchor="left">
-        <Typography>
-          <Icon className="fas fa-rocket" />
-          Welcome, Ryan
-        </Typography>
-        <List>
-          <ListItem>Dashboard</ListItem>
-          <ListItem>Profile</ListItem>
-          <ListItem>Subscription</ListItem>
-        </List>
-      </Drawer>
+      <Sidebar />
     </>
   );
 };
