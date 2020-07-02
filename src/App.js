786696ea99ea66a846d8e7dc88dba571a3a5c0c5
Leon <leon.karho.chung@gmail.com>
2020-07-02 13:01:29 -0400

    normalize css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Dashboard />
     </div>
   );
