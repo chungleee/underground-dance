7e3b5da49d00924a6a439294d1d05d3f52dd915c
Leon <leon.karho.chung@gmail.com>
2020-07-02 17:33:55 -0400

    fetch data and pass it to component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Box,
@@ -9,26 +9,8 @@ import {
   Tab,
   Divider,
 } from "@material-ui/core";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+import TabPanel from "./presentational/TabPanel";
+import RocketInfoCard from "./presentational/RocketInfoCard";
 
 function a11yProps(index) {
   return {
@@ -47,10 +29,55 @@ const useStyles = makeStyles({
 const Main = () => {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  const [launches, setLaunches] = useState([]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    fetch("https://api.spacex.land/graphql/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+          launches(limit: 3) {
+            rocket {
+              rocket {
+                cost_per_launch
+                description
+                diameter {
+                  meters
+                }
+                height {
+                  meters
+                }
+                name
+              }
+              rocket_name
+            }
+            launch_site {
+              site_name_long
+            }
+            links {
+              flickr_images
+            }
+          }
+        }    
+        `,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data.data);
+        setLaunches(data.data.launches);
+      });
+  }, []);
   return (
     <>
       <Box style={{ width: "75%" }}>
@@ -68,6 +95,7 @@ const Main = () => {
         </Container>
         <TabPanel value={value} index={0}>
           <Typography>Active Rockets</Typography>
+          <RocketInfoCard launches={launches} />
         </TabPanel>
         <TabPanel value={value} index={1}>
           <Typography>Satellites</Typography>
