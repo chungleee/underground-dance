b92e305873a908c2dcdc36da8326b9c75b41e5c3
Leon <leon.karho.chung@gmail.com>
2020-07-02 16:04:45 -0400

    layout styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import Main from "../components/Main";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    height: "100vh",
+  },
+});
 const Dashboard = () => {
+  const classes = useStyles();
   return (
-    <>
+    <div className={classes.root}>
       <Sidebar />
       <Main />
-    </>
+    </div>
   );
 };
 
